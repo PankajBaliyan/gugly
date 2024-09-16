3a8b4a5e5b3d13e231eae9259fcfcd482f22e577
Pankaj Kumar <pankajbaliyan90@gmail.com>
2024-09-16 22:04:38 +0530

    fix nav reload issue

--- src/Doctor/NavStaff.jsx
+++ src/Doctor/NavStaff.jsx
@@ -1,19 +1,20 @@
-import * as React from 'react';
-import PropTypes from 'prop-types';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import CssBaseline from '@mui/material/CssBaseline';
-import Divider from '@mui/material/Divider';
-import Drawer from '@mui/material/Drawer';
-import IconButton from '@mui/material/IconButton';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemText from '@mui/material/ListItemText';
-import MenuIcon from '@mui/icons-material/Menu';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import { autocompleteClasses } from '@mui/material';
+import * as React from "react";
+import PropTypes from "prop-types";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import Divider from "@mui/material/Divider";
+import Drawer from "@mui/material/Drawer";
+import IconButton from "@mui/material/IconButton";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import MenuIcon from "@mui/icons-material/Menu";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import { autocompleteClasses } from "@mui/material";
+import { Link } from "react-router-dom";
 
 const drawerWidth = 200;
 
@@ -21,7 +22,6 @@ function NavStaff(props) {
   const { window } = props;
   const [mobileOpen, setMobileOpen] = React.useState(false);
   const [isClosing, setIsClosing] = React.useState(false);
-  
 
   const handleDrawerClose = () => {
     setIsClosing(true);
@@ -44,13 +44,12 @@ function NavStaff(props) {
 
       <Typography
         variant="h3"
-
         sx={{
           fontFamily: "Playfair Display",
           fontOpticalSizing: "auto",
           fontWeight: 400,
           fontStyle: "normal",
-          ml: '1rem'
+          ml: "1rem",
         }}
       >
         gugly
@@ -58,13 +57,13 @@ function NavStaff(props) {
       <br></br>
 
       <List>
-        {['PatientDetails', 'Medicines', 'Symptoms', 'Diagnosis', 'Instructions', 'PatientsSummary', 'Password', 'Logout'].map((text, index) => (
-
+        {["PatientDetails", "Medicines", "Symptoms", "Diagnosis", "Instructions", "PatientsSummary", "Password", "Logout"].map((text, index) => (
           <ListItem key={text} disablePadding>
-            <ListItemButton to= {`/${text.toLowerCase()}`}>
-             
-              <ListItemText primary={text} />
-            </ListItemButton>
+            <Link to={`/${text.toLowerCase()}`} style={{ textDecoration: "none", color: "black" }}>
+              <ListItemButton>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </Link>
           </ListItem>
         ))}
       </List>
@@ -75,13 +74,13 @@ function NavStaff(props) {
   const container = window !== undefined ? () => window().document.body : undefined;
 
   return (
-    <Box sx={{ display: 'flex' }}>
+    <Box sx={{ display: "flex" }}>
       <CssBaseline />
       <AppBar
         position="fixed"
         sx={{
           // width: { sm: `calc(100% - ${drawerWidth}px)` },
-          width: { md: `calc(100% - ${drawerWidth}px)`, xs:'100%' },
+          width: { md: `calc(100% - ${drawerWidth}px)`, xs: "100%" },
           // ml: { sm: `${drawerWidth}px` },
           ml: { md: `${drawerWidth}px`, xs: 0 },
         }}
@@ -93,7 +92,7 @@ function NavStaff(props) {
             edge="start"
             onClick={handleDrawerToggle}
             // sx={{ mr: 2, display: { sm: 'none' } }}
-            sx={{ mr: 2, display: { md: 'none' } }}
+            sx={{ mr: 2, display: { md: "none" } }}
           >
             <MenuIcon />
           </IconButton>
@@ -102,11 +101,7 @@ function NavStaff(props) {
           </Typography>
         </Toolbar>
       </AppBar>
-      <Box
-        component="nav"
-        sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
-        aria-label="mailbox folders"
-      >
+      <Box component="nav" sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }} aria-label="mailbox folders">
         {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
         <Drawer
           container={container}
@@ -119,8 +114,8 @@ function NavStaff(props) {
           }}
           sx={{
             //  display: { xs: 'block', sm: 'none' },
-            display: { xs: 'block', md: 'none' },
-            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+            display: { xs: "block", md: "none" },
+            "& .MuiDrawer-paper": { boxSizing: "border-box", width: drawerWidth },
           }}
         >
           {drawer}
@@ -129,21 +124,16 @@ function NavStaff(props) {
           variant="permanent"
           sx={{
             // display: { xs: 'none', sm: 'block' },
-            display: { xs: 'none', md: 'block' },
-            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+            display: { xs: "none", md: "block" },
+            "& .MuiDrawer-paper": { boxSizing: "border-box", width: drawerWidth },
           }}
           open
         >
           {drawer}
         </Drawer>
       </Box>
-      <Box
-        component="main"
-        sx={{ flexGrow: 1, p: 3, width: { sm: `calc(100% - ${drawerWidth}px)` } }}
-      >
+      <Box component="main" sx={{ flexGrow: 1, p: 3, width: { sm: `calc(100% - ${drawerWidth}px)` } }}>
         <Toolbar />
-       
-
       </Box>
     </Box>
   );
